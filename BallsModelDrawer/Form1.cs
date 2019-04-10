19ed3568bbe59c31853b537bca3c2b61b959f54e
levabala <levabal@ya.ru>
2019-04-10 08:51:00 +0300

    updates

--- BallsModelDrawer/Form1.cs
+++ BallsModelDrawer/Form1.cs
@@ -13,14 +13,29 @@ namespace BallsModelDrawer
     public partial class Form1 : Form
     {                
         List<BallsModel.Point> localPoints = new List<BallsModel.Point>(){
-            new BallsModel.Point(1, 1),
-            new BallsModel.Point(3, 2),
-            new BallsModel.Point(2, 5),
-            new BallsModel.Point(5, 10),
-            new BallsModel.Point(-2, 12),
+            //new BallsModel.Point(1, 1),
+            //new BallsModel.Point(3, 2),
+            //new BallsModel.Point(2, 5),
+            //new BallsModel.Point(5, 10),
+            //new BallsModel.Point(-2, 12),
         };
         BallsModel.Point[] points;
         BallsModel.Polygone poly;
+        BallsModel.Ball ball1 = new BallsModel.Ball(
+            new BallsModel.Vector(2, 2, 1, 0),
+            1,
+            new BallsModel.Circle(2, 2, 2)
+        );
+        BallsModel.Ball ball2 = new BallsModel.Ball(
+            new BallsModel.Vector(4, 2, -1, 0),
+            1,
+            new BallsModel.Circle(4, 2, 2)
+        );
+
+
+
+        List<BallsModel.Ball> balls;
+
         bool polyIsEmpty = false;
         float scale = 1;
         float translateX = 0;
@@ -30,6 +45,7 @@ namespace BallsModelDrawer
         {
             InitializeComponent();
 
+            balls = new List<BallsModel.Ball>() { ball1, ball2 };
             rebuildData();
         }
 
@@ -76,8 +92,8 @@ namespace BallsModelDrawer
         {
             localPoints.Add(
                 new BallsModel.Point(
-                    (decimal)((e.X - translateX) / scale), 
-                    (decimal)((e.Y - translateY) / scale)
+                    (e.X - translateX) / scale, 
+                    (e.Y - translateY) / scale
                 )
             );
             rebuildData();
@@ -107,59 +123,85 @@ namespace BallsModelDrawer
             g.DrawLine(axisPen, -ClientSize.Width, 0, ClientSize.Width, 0);
             g.DrawLine(axisPen, 0, -ClientSize.Height, 0, ClientSize.Height);
 
-            if (polyIsEmpty)
-                return;
+            if (!polyIsEmpty)
+            {
 
-            // draw lines
-            Pen linePen = new Pen(Color.LightPink, 0.3f);
-            poly.lines.ToList().ForEach(
-                line => g.DrawLine(
-                    linePen,
-                    (float)line.x1,
-                    (float)line.y1,
-                    (float)line.x2,
-                    (float)line.y2
-                )
-            );
+                // draw lines
+                Pen linePen = new Pen(Color.LightPink, 0.3f);
+                poly.lines.ToList().ForEach(
+                    line => g.DrawLine(
+                        linePen,
+                        (float)line.x1,
+                        (float)line.y1,
+                        (float)line.x2,
+                        (float)line.y2
+                    )
+                );
 
-            // draw points
-            const float pointSize = 0.8f;
-            poly.points.ToList().ForEach(
-                point => g.FillEllipse(
-                    Brushes.DarkRed,
-                    (float)point.x - pointSize / 2,
-                    (float)point.y - pointSize / 2,
-                    pointSize,
-                    pointSize
-                )
-            );
-
-            // draw normals
-            Pen normalPen = new Pen(Color.DarkViolet, 0.3f);
-            normalPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
-            poly.normals.ToList().ForEach(
-                normal =>
-                {
-                    BallsModel.Point vStart = normal.StartPoint;
-                    BallsModel.Point vEnd = normal.EndPoint;
-
-                    g.DrawLine(
-                        normalPen,
-                        (float)normal.x,
-                        (float)normal.y,
-                        (float)normal.ex,
-                        (float)normal.ey
-                    );
-
-                    g.FillEllipse(
-                        Brushes.DarkSeaGreen,
-                        (float)vStart.x - pointSize / 2,
-                        (float)vStart.y - pointSize / 2,
+                // draw points
+                const float pointSize = 0.8f;
+                poly.points.ToList().ForEach(
+                    point => g.FillEllipse(
+                        Brushes.DarkRed,
+                        (float)point.x - pointSize / 2,
+                        (float)point.y - pointSize / 2,
                         pointSize,
                         pointSize
-                    );
-                }
-            );
+                    )
+                );
+
+                // draw normals
+                Pen normalPen = new Pen(Color.DarkViolet, 0.3f);
+                normalPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+                poly.normals.ToList().ForEach(
+                    normal =>
+                    {
+                        BallsModel.Point vStart = normal.StartPoint;
+                        BallsModel.Point vEnd = normal.EndPoint;
+
+                        g.DrawLine(
+                            normalPen,
+                            (float)normal.x,
+                            (float)normal.y,
+                            (float)normal.ex,
+                            (float)normal.ey
+                        );
+
+                        g.FillEllipse(
+                            Brushes.DarkSeaGreen,
+                            (float)vStart.x - pointSize / 2,
+                            (float)vStart.y - pointSize / 2,
+                            pointSize,
+                            pointSize
+                        );
+                    }
+                );
+
+            }
+
+            // draw balls
+            Pen ballPen = new Pen(Color.Red, 0.2f);
+            Pen ballVectorPen = new Pen(Color.Brown, 0.2f);
+            ballVectorPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            //ballPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;            
+            balls.ForEach(ball =>
+            {
+                g.DrawEllipse(
+                    ballPen,
+                    (float)(ball.frame.x - ball.frame.radius / 2),
+                    (float)(ball.frame.y - ball.frame.radius / 2),
+                    (float)(ball.frame.radius),
+                    (float)(ball.frame.radius)
+                );
+
+                g.DrawLine(
+                    ballVectorPen,
+                    (float)ball.ph.v.x,
+                    (float)ball.ph.v.y,
+                    (float)ball.ph.v.ex,
+                    (float)ball.ph.v.ey
+                );
+            });
         }
-    }
+    }    
 }
